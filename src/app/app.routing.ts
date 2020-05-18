31615d77a9dcb36aaf5aa68727f3e6ff2f7a029c
Elving96 <Elving96@users.noreply.github.com>
2020-05-17 18:15:12 -0600

    lab2 terminado

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,10 @@ import{HomeComponent} from './components/home/home.component';
 import{LoginComponent} from './components/login/login.component';
 import{ErrorComponent} from './components/error/error.component';
 import{RegisterComponent} from './components/register/register.component';
+//lab2
+import{UsersComponent} from './components/users/users.component';
+import{CategoriesComponent} from './components/categories/categories.component';
+import{PostsComponent} from './components/posts/posts.component';
 
 
 const appRoutes: Routes=[
@@ -12,6 +16,10 @@ const appRoutes: Routes=[
   {path:'inicio',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'registro',component:RegisterComponent},
+  //Lab2
+  {path:'users',component:UsersComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'posts',component:PostsComponent},
 
   {path:'**',component:ErrorComponent}
 ];
