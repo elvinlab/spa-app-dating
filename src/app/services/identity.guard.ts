60d287c4278184aa361f6f7829abe9e514f41338
Elving96 <Elving96@users.noreply.github.com>
2020-06-07 12:26:16 -0600

    agregando roles entity.guard

--- src/app/services/identity.guard.ts
+++ src/app/services/identity.guard.ts
@@ -1,24 +1,29 @@
 import {Injectable} from '@angular/core';
 import {Router, CanActivate} from '@angular/router';
 import {ClientService} from './client.service';
+import {CommerceService} from './commerce.service';
 
 @Injectable()
 export class IdentityGuard implements CanActivate{
 
 	constructor(
 		private _router: Router,
-		private _ClientService: ClientService
+		private _ClientService: ClientService,
+		private _CommerceService: CommerceService
 	){}
 
 	canActivate(){
-		let identity = this._ClientService.getIdentity();
+		let identityClient   = this._ClientService.getIdentity();
+		let identityCommerce = this._CommerceService.getIdentity();
 
-		if(identity){
+		if(identityClient){
 			return true;
+		}else if(identityCommerce){
+			return true;
+
 		}else{
 			this._router.navigate(['/error']);
 			return false;
 		}
-
 	}
 }
