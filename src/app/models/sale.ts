2e1afcace31e6bb856755097201cb3e995bcc318
Elving96 <Elving96@users.noreply.github.com>
2020-06-08 13:26:00 -0600

    eliminando ventas

--- src/app/models/sale.ts
+++
@@ -1,9 +0,0 @@
-export class Sale {
-    constructor(
-        public id: number,
-        public commerce_id: string,
-        public service_id: string,
-        public amount: number,
-        public iva: number,
-    ) { }
-}
