c3e18fdd8f36dff8e3f60fa0d45d1a1827d6ced2
Elving96 <Elving96@users.noreply.github.com>
2020-06-08 13:01:23 -0600

    pug en editar promocion cantidad > a max

--- src/app/components/promotion-edit/promotion-edit.component.ts
+++ src/app/components/promotion-edit/promotion-edit.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { CommerceService } from '../../services/commerce.service';
 import { PromotionService } from '../../services/promotion.service';
 import { Promotion } from '../../models/promotion';
-import { NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 import { global } from '../../services/global';
 
 @Component({
-  selector: 'app-promotion-edit',
-  templateUrl: './promotion-edit.component.html',
-  styleUrls: ['./promotion-edit.component.css'],
-  providers: [CommerceService, PromotionService]
+	selector: 'app-promotion-edit',
+	templateUrl: './promotion-edit.component.html',
+	styleUrls: ['./promotion-edit.component.css'],
+	providers: [CommerceService, PromotionService]
 })
 export class PromotionEditComponent implements OnInit {
 	public page_title: string;
@@ -22,55 +22,55 @@ export class PromotionEditComponent implements OnInit {
 	public url;
 	dateSelected;
 	date: { year: number, month: number };
-  constructor(
-    private _route: ActivatedRoute,
+	constructor(
+		private _route: ActivatedRoute,
 		private _router: Router,
 		private _commerceService: CommerceService,
-    private _promotionService: PromotionService,
-    private calendar: NgbCalendar,
-  ) {
-    this.page_title = "Editar Promocion";
-    this.identity = this._commerceService.getIdentity();
-      this.token = this._commerceService.getToken();
-      this.promotion = new Promotion(1, '','',0,0,'','','',0);
-      this.minDate = this.calendar.getToday();
-   }
+		private _promotionService: PromotionService,
+		private calendar: NgbCalendar,
+	) {
+		this.page_title = "Editar Promocion";
+		this.identity = this._commerceService.getIdentity();
+		this.token = this._commerceService.getToken();
+		this.promotion = new Promotion(1, '', '', 0, 0, '', '', '', 0);
+		this.minDate = this.calendar.getToday();
+	}
 
 
-   public froala_options: Object = {
+	public froala_options: Object = {
 		charCounterCount: true,
 		language: 'es',
 		toolbarButtons: ['bold', 'italic', 'underline', 'paragraphFormat'],
 		toolbarButtonsXS: ['bold', 'italic', 'underline', 'paragraphFormat'],
 		toolbarButtonsSM: ['bold', 'italic', 'underline', 'paragraphFormat'],
 		toolbarButtonsMD: ['bold', 'italic', 'underline', 'paragraphFormat'],
-    }
+	}
 
-    public afuConfig = {
-      multiple: false,
-      formatsAllowed: ".jpg, .png, .gif, .jpeg",
-      maxSize: "50",
-      uploadAPI: {
-        url: global.url + 'promotion/upload',
-        headers: {
-          "Authorization": this._commerceService.getToken()
-        }
-      },
-  
-      theme: "attachPin",
-      hideProgressBar: false,
-      hideResetBtn: true,
-      hideSelectBtn: false,
-      attachPinText: 'Sube tu avatar de cliente',
-  
-    };
+	public afuConfig = {
+		multiple: false,
+		formatsAllowed: ".jpg, .png, .gif, .jpeg",
+		maxSize: "50",
+		uploadAPI: {
+			url: global.url + 'promotion/upload',
+			headers: {
+				"Authorization": this._commerceService.getToken()
+			}
+		},
 
-  ngOnInit(): void {
-    this.getPromotion();
-  }
+		theme: "attachPin",
+		hideProgressBar: false,
+		hideResetBtn: true,
+		hideSelectBtn: false,
+		attachPinText: 'Sube tu avatar de cliente',
+
+	};
+
+	ngOnInit(): void {
+		this.getPromotion();
+	}
 
 
-  getPromotion() {
+	getPromotion() {
 		this._route.params.subscribe(params => {
 			let id = +params['id'];
 
@@ -80,18 +80,18 @@ export class PromotionEditComponent implements OnInit {
 					if (response.status == 'success') {
 						let promotion = response.promotion;
 
-				
+
 						if (promotion[0].commerce_id != this.identity.id) {
 							this._router.navigate(['/../listar-promociones']);
 						} else {
 							this.promotion = new Promotion(
 								promotion[0].id,
-                promotion[0].commerce_id,
-                promotion[0].coupon,
-                promotion[0].max,
-                promotion[0].amount,
-                promotion[0].expiry,
-                promotion[0].description,
+								promotion[0].commerce_id,
+								promotion[0].coupon,
+								promotion[0].max,
+								promotion[0].amount,
+								promotion[0].expiry,
+								promotion[0].description,
 								promotion[0].image,
 								promotion[0].discount,
 
@@ -110,13 +110,13 @@ export class PromotionEditComponent implements OnInit {
 	}
 
 
-  onSubmit(form) {
+	onSubmit(form) {
+
 
 		this.promotion.expiry = this.dateSelected.year + "-" + this.dateSelected.month + "-" + this.dateSelected.day;
-
 		console.log(this.promotion.expiry);
 		console.log(this.dateSelected);
-		this._promotionService.update(this.token, this.promotion,  this.promotion.id).subscribe(
+		this._promotionService.update(this.token, this.promotion, this.promotion.id).subscribe(
 			response => {
 				if (response.status == 'success') {
 					this.promotion = response.category;
@@ -135,7 +135,7 @@ export class PromotionEditComponent implements OnInit {
 	}
 
 
-  imageUpload(data){
+	imageUpload(data) {
 		let image_data = JSON.parse(data.response);
 		this.promotion.image = image_data.image;
 	}
