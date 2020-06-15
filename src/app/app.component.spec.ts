5effefa8481a5ed32a4c42ddb67e94903153b262
Elving96 <Elving96@users.noreply.github.com>
2020-06-15 01:33:11 -0600

    reparar bugs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog-frontend'`, () => {
+  it(`should have as title 'cita-online'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog-frontend');
+    expect(app.title).toEqual('cita-online');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('blog-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cita-online app is running!');
   });
 });
--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var global = {
-	url: 'https://api-app-dating.herokuapp.com/api/',
+	url: 'http://127.0.0.1:8000/api/',
 	htmlEntities: function(str) {
       return String(str).replace('&ntilde;', 'ñ')
                         .replace('&Ntilde;', 'Ñ')
